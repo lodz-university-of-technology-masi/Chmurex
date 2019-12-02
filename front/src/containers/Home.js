8b01b8d49eee8803d2460053378eab88737d99ee
JKWrobel <bociek428@gmail.com>
2019-12-02 19:48:18 +0100

    Added version to home page

--- front/src/containers/Home.js
+++ front/src/containers/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="Home">
             <div className="lander">
-                <h1>Recruit</h1>
+                <h1>Recruit v0.2</h1>
                 <p>A recruitment app brought by Chmurex</p>
             </div>
         </div>
