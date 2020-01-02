9000981e3c30aaa1e8844944b236d5fc810bf8f4
GabroVV <origingabriel3124@gmail.com>
2020-01-02 21:59:44 +0100

    AddCandidate lambda

--- back/LambdaLogic/src/main/java/CandidateLambdas/AddCandidate.java
+++ back/LambdaLogic/src/main/java/CandidateLambdas/AddCandidate.java
@@ -1,4 +1,46 @@
 package CandidateLambdas;
 
-public class AddCandidate {
+
+import Common.GatewayResponse;
+import Model.AddCandidateRequest;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
+import com.amazonaws.services.cognitoidp.model.*;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+public class AddCandidate implements RequestHandler<AddCandidateRequest, GatewayResponse> {
+    private AWSCognitoIdentityProvider cognito = AWSCognitoIdentityProviderClientBuilder.defaultClient();
+
+    public GatewayResponse handleRequest(AddCandidateRequest rq, Context context) {
+        AdminCreateUserRequest createUserRequest = new AdminCreateUserRequest()
+                .withUserPoolId("us-east-1_Cb75QZCzu")
+                .withUsername(rq.getEmail())
+                .withUserAttributes(
+                    new AttributeType()
+                        .withName("email")
+                        .withValue(rq.getEmail()),
+                    new AttributeType()
+                            .withName("custom:isRecruiter")
+                            .withValue("0"),
+                    new AttributeType()
+                            .withName("email_verified")
+                            .withValue("true")
+                );
+        AdminCreateUserResult createUserResult =  cognito.adminCreateUser(createUserRequest);
+        AdminSetUserPasswordRequest passwordRequest = new AdminSetUserPasswordRequest()
+                .withUserPoolId("us-east-1_Cb75QZCzu")
+                .withUsername(rq.getEmail())
+                .withPassword(rq.getPassword())
+                .withPermanent(true);
+        AdminSetUserPasswordResult passwordResult = cognito.adminSetUserPassword(passwordRequest);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+        return new GatewayResponse("User " + rq.getEmail() +" created", headers, 200);
+    }
+
 }
