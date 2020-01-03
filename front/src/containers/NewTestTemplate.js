f98a1ed5629c9be18c53fffdf6f083e20b59c3df
Przemysław Komuda <p.komuda1@gmail.com>
2020-01-03 16:37:52 +0100

    Final version of adding new test (with removing questions and answers)

--- front/src/containers/NewTestTemplate.js
+++ front/src/containers/NewTestTemplate.js
@@ -1,23 +1,18 @@
 import React from "react"
-import { Button, Form, FormGroup, ControlLabel, FormControl } from "react-bootstrap";
+import { Button, Form, FormControl, FormGroup, InputGroup } from "react-bootstrap";
 
 class NewTestTemplate extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {count: 0, id: "", types: [], texts: [], answers: []};
-
-        this.handleChangeId = this.handleChangeId.bind(this);
-        this.handleChangeType = this.handleChangeType.bind(this);
-        this.handleChangeText = this.handleChangeText.bind(this);
-        this.handleChangeAnswer = this.handleChangeAnswer.bind(this);
-        this.handleChangeMultipleAnswer = this.handleChangeMultipleAnswer.bind(this);
-        this.handleAddQuestion = this.handleAddQuestion.bind(this);
-        this.handleAddAnswer = this.handleAddAnswer.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {count: 0, id: "", language: "EN", types: [], texts: [], answers: []};
     }
 
     handleChangeId(event) {
         this.setState({id: event.target.value});
+    }
+
+    handleChangeLanguage(event) {
+        this.setState({language: event.target.value});
     }
 
     handleChangeType(event, i) {
@@ -66,6 +61,22 @@ class NewTestTemplate extends React.Component {
         this.setState({count: this.state.count + 1});
     }
 
+    handleDeleteQuestion(i) {
+        let tempTypes = [...this.state.types];
+        tempTypes.splice(i, 1);
+        this.setState({types: tempTypes});
+
+        let tempTexts = [...this.state.texts];
+        tempTexts.splice(i, 1);
+        this.setState({texts: tempTexts});
+
+        let tempAnswers = [...this.state.answers];
+        tempAnswers.splice(i, 1);
+        this.setState({answers: tempAnswers});
+
+        this.setState({count: this.state.count - 1});
+    }
+
     handleAddAnswer(i) {
         let tempAnswers = [...this.state.answers];
         if (Array.isArray(tempAnswers[i])) {
@@ -76,27 +87,70 @@ class NewTestTemplate extends React.Component {
         this.setState({answers: tempAnswers});
     }
 
+    handleDeleteAnswer(i, j) {
+        let tempAnswers = [...this.state.answers];
+        tempAnswers[i].splice(j, 1);
+        this.setState({answers: tempAnswers});
+    }
+
+    validateInput() {
+        if (this.state.count === 0 || this.state.id.trim() === "") {
+            return false;
+        }
+
+        let textsOk = true;
+        for (let i = 0; i < this.state.texts.length; i++) {
+            if (this.state.texts[i].trim() === "") {
+                textsOk = false;
+                return false;
+            }
+        }
+
+        let answersOk = true;
+        for (let i = 0; i < this.state.answers.length; i++) {
+            if (Array.isArray(this.state.answers[i])) {
+                for (let j = 0; j < this.state.answers[i].length; j++) {
+                    if (this.state.answers[i][j].trim() === "") {
+                        answersOk = false;
+                        return false;
+                    }
+                }
+            } else if (this.state.answers[i].trim() === "") {
+                answersOk = false;
+                return false;
+            }
+        }
+
+        return textsOk && answersOk;
+    }
+
     handleSubmit(event) {
         event.preventDefault();
-        let contents = {};
-        contents[this.state.id] = {"questions": []};
-        for (let i = 0; i < this.state.count; i++) {
-            contents[this.state.id]["questions"].push({"question": {
-                                                            "type": this.state.types[i],
-                                                            "text": this.state.texts[i],
-                                                            "answers": this.state.answers[i]
-                                                            }
-                                                      });
-        }
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                alert("Successfully saved new test template");
+        if (!this.validateInput()) {
+            alert("Every field must contain a value");
+        } else {
+            let contents = {};
+            let name = "Questions" + this.state.id + this.state.language;
+            contents[name] = {"questions": []};
+            for (let i = 0; i < this.state.count; i++) {
+                contents[name]["questions"].push({
+                    "question": {
+                        "type": this.state.types[i],
+                        "text": this.state.texts[i],
+                        "answers": this.state.answers[i]
+                    }
+                });
             }
-        };
-        xhr.open("POST", "https://lrjyi691l7.execute-api.us-east-1.amazonaws.com/Prod/recruiter/newtesttemplate");
-        xhr.send(JSON.stringify({"ID": this.state.id, "JSON": JSON.stringify(contents, null, 4)}));
-        this.setState({count: 0, id: "", types: [], texts: [], answers: []});
+            let xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    alert("Successfully saved new test template");
+                }
+            };
+            xhr.open("POST", "https://lrjyi691l7.execute-api.us-east-1.amazonaws.com/Prod/recruiter/newtesttemplate");
+            xhr.send(JSON.stringify({"ID": this.state.id + this.state.language, "JSON": JSON.stringify(contents, null, 4)}));
+            this.setState({count: 0, id: "", language: "EN", types: [], texts: [], answers: []});
+        }
     }
 
     renderForm() {
@@ -104,20 +158,27 @@ class NewTestTemplate extends React.Component {
         for (let i = 0; i < this.state.count; i++) {
             questions.push(
                 <div key={i}>
-                    <FormGroup bsSize="large">
-                        <ControlLabel>Question {i + 1} type</ControlLabel>
-                        <FormControl componentClass="select" value={this.state.types[i]} onChange={(event) => this.handleChangeType(event, i)}>
-                            <option value="open">Open</option>
-                            <option value="numeric">Numeric</option>
-                            <option value="choice">Choice</option>
-                        </FormControl>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon>Question {i + 1} type</InputGroup.Addon>
+                            <FormControl componentClass="select" value={this.state.types[i]} onChange={(event) => this.handleChangeType(event, i)}>
+                                <option value="open">Open</option>
+                                <option value="numeric">Numeric</option>
+                                <option value="choice">Choice</option>
+                            </FormControl>
+                            <InputGroup.Button onClick={() => this.handleDeleteQuestion(i)}>
+                                <Button style={{backgroundColor: "#00C3ED", color: "#FFFFFF", fontWeight: "bold"}}>Delete question</Button>
+                            </InputGroup.Button>
+                        </InputGroup>
                     </FormGroup>
-                    <FormGroup bsSize="large">
-                        <ControlLabel>Question {i + 1}</ControlLabel>
-                        <FormControl type="text" value={this.state.texts[i]} onChange={(event) => this.handleChangeText(event, i)}/>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon>Question {i + 1}</InputGroup.Addon>
+                            <FormControl type="text" value={this.state.texts[i]} onChange={(event) => this.handleChangeText(event, i)}/>
+                        </InputGroup>
                     </FormGroup>
                     {this.renderAnswer(i)}
-                    <hr style={{color: "black", backgroundColor: "black", height: 2}}/>
+                    <hr style={{backgroundColor: "#333333", height: 2}}/>
                 </div>
             );
         }
@@ -128,16 +189,20 @@ class NewTestTemplate extends React.Component {
         switch (this.state.types[i]) {
             case "open":
                 return (
-                    <FormGroup bsSize="large">
-                        <ControlLabel>Answer {i + 1}</ControlLabel>
-                        <FormControl type="text" value={this.state.answers[i]} onChange={(event) => this.handleChangeAnswer(event, i)}/>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon>Answer {i + 1}</InputGroup.Addon>
+                            <FormControl type="text" value={this.state.answers[i]} onChange={(event) => this.handleChangeAnswer(event, i)}/>
+                        </InputGroup>
                     </FormGroup>
                 );
             case "numeric":
                 return (
-                    <FormGroup bsSize="large">
-                        <ControlLabel>Answer {i + 1}</ControlLabel>
-                        <FormControl type="number" value={this.state.answers[i]} onChange={(event) => this.handleChangeAnswer(event, i)}/>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon>Answer {i + 1}</InputGroup.Addon>
+                            <FormControl type="number" value={this.state.answers[i]} onChange={(event) => this.handleChangeAnswer(event, i)}/>
+                        </InputGroup>
                     </FormGroup>
                 );
             case "choice":
@@ -145,16 +210,21 @@ class NewTestTemplate extends React.Component {
                 for (let j = 0; j < this.state.answers[i].length; j++) {
                     answers.push(
                         <div key={i + " " + j}>
-                            <FormGroup bsSize="large">
-                                <ControlLabel>Answer {i + 1}.{j + 1}</ControlLabel>
-                                <FormControl type="text" value={this.state.answers[i][j]} onChange={(event) => this.handleChangeMultipleAnswer(event, i, j)}/>
+                            <FormGroup>
+                                <InputGroup>
+                                    <InputGroup.Addon>Answer {i + 1}.{j + 1}</InputGroup.Addon>
+                                    <FormControl type="text" value={this.state.answers[i][j]} onChange={(event) => this.handleChangeMultipleAnswer(event, i, j)}/>
+                                    <InputGroup.Button onClick={() => this.handleDeleteAnswer(i, j)}>
+                                        <Button style={{backgroundColor: "#00C3ED", color: "#FFFFFF", fontWeight: "bold"}}>Delete answer</Button>
+                                    </InputGroup.Button>
+                                </InputGroup>
                             </FormGroup>
                         </div>
                     );
                 }
                 answers.push(
                     <div key={this.state.answers[i].length + 1}>
-                        <Button block bsSize="large" onClick={() => this.handleAddAnswer(i)}>
+                        <Button style={{backgroundColor: "#00C3ED", color: "#FFFFFF", fontWeight: "bold"}} onClick={() => this.handleAddAnswer(i)}>
                             New answer
                         </Button>
                     </div>
@@ -168,17 +238,29 @@ class NewTestTemplate extends React.Component {
     render() {
         return(
             <div className="NewTestTemplate">
-                <h1>Create test</h1>
-                <Form onSubmit={this.handleSubmit}>
-                    <FormGroup bsSize="large">
-                        <ControlLabel>Test name</ControlLabel>
-                        <FormControl type="text" value={this.state.id} onChange={this.handleChangeId}/>
+                <h1>Create a new test</h1>
+                <Form onSubmit={(event) => this.handleSubmit(event)}>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon>Test name</InputGroup.Addon>
+                            <FormControl type="text" value={this.state.id} onChange={(event) => this.handleChangeId(event)}/>
+                        </InputGroup>
                     </FormGroup>
+                    <FormGroup>
+                        <InputGroup>
+                            <InputGroup.Addon>Language</InputGroup.Addon>
+                            <FormControl componentClass="select" value={this.state.language} onChange={(event) => this.handleChangeLanguage(event)}>
+                                <option value="EN">English</option>
+                                <option value="PL">Polish</option>
+                            </FormControl>
+                        </InputGroup>
+                    </FormGroup>
+                    <hr style={{backgroundColor: "#333333", height: 2}}/>
                     {this.renderForm()}
-                    <Button block bsSize="large" onClick={this.handleAddQuestion}>
+                    <Button onClick={() => this.handleAddQuestion()} style={{display: "inline-block", backgroundColor: "#00C3ED", color: "#FFFFFF", fontWeight: "bold"}}>
                         New question
                     </Button>
-                    <Button block bsSize="large" type="submit">
+                    <Button type="submit" style={{display: "inline-block", backgroundColor: "#00C3ED", color: "#FFFFFF", fontWeight: "bold"}}>
                         Submit
                     </Button>
                 </Form>
@@ -188,41 +270,3 @@ class NewTestTemplate extends React.Component {
 }
 
 export default NewTestTemplate
-
-/*
-{
-    "QuestionsTest1EN": {
-        "questions": [
-            {
-                "question": {
-                    "type": "O",
-                    "text": "Wypowiedz sie",
-                    "answers": [
-
-                    ]
-                }
-            },
-            {
-                "question": {
-                    "type": "W",
-                    "text": "Która z tych odpowiedzi",
-                    "answers": [
-                        "a",
-                        "b",
-                        "c"
-                    ]
-                }
-            },
-            {
-                "question": {
-                    "type": "L",
-                    "text": "Podaj wartosc liczbowa",
-                    "answers": [
-
-                    ]
-                }
-            }
-        ]
-    }
-}
- */
