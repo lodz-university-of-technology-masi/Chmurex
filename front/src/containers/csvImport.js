f09457c0f05bd750aecdc61441ecb5e6da4710f3
GabroVV <origingabriel3124@gmail.com>
2020-01-05 02:24:30 +0100

    csv reader displays loaded file

--- front/src/containers/csvImport.js
+++ front/src/containers/csvImport.js
@@ -1,18 +1,28 @@
 import React from "react";
 import {Button} from "react-bootstrap";
+import CSVReader from 'react-csv-reader'
 class CsvImport extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            data:'',
+        }
+        this.fileLoaded = this.fileLoaded.bind(this);
     }
     render() {
         return (
             <div className="CsvImport">
                <h1>
-                    Import test template from csv file
+                    Import test template from csvv file
                </h1>
-
+               <CSVReader onFileLoaded={this.fileLoaded} />
+               <p>{this.state.data}</p>
             </div>
             );
+    }
+
+    fileLoaded(data) {
+        this.setState({data:data})
     }
 }
 
